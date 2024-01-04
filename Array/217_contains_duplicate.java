1077e8144914d91e9abbf2671c8cc366d6c435cd
Chao Li <learnerchao@gmail.com>
2024-01-04 10:38:40 -0800

    add .gitignore and leetcode 217

---
+++ Array/217_contains_duplicate.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        Set<Integer> unique = new HashSet();
+        for (int num : nums) {
+            if (!unique.add(num)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
